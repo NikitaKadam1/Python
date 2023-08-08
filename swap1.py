bdfacbd6498b4bb7f46203ffd3cbe9b8f8e1e6db
NikitaKadam1 <141641089+NikitaKadam1@users.noreply.github.com>
2023-08-08 20:48:04 +0530

    Create swap1.py
    program for swaping 2 numbers

---
+++ swap1.py
@@ -0,0 +1,9 @@
+print("\n\n\t\t\t swapping")
+
+a=int(input("\n\tenter a:"))
+b=int(input("\n\tenter b:"))
+c=a
+a=b
+b=c
+print("after swapping value of a:",a)
+print("after swapping value of b:",b)
