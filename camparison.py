aa0cfbb6d87a175a570a76f8b69f4bfa8eb09f08
NikitaKadam1 <141641089+NikitaKadam1@users.noreply.github.com>
2023-08-08 20:43:11 +0530

    Create camparison.py
    program for find greater number from 3 numbers

---
+++ camparison.py
@@ -0,0 +1,10 @@
+print("\n\t\t\t\t\t camparison")
+a=int(input("\nenter no a:"))
+b=int(input("enter no b:"))
+c=int(input("enter no c:"))
+if a>b and a>c:
+ print("a is a greater number")
+if b>a and b>c:
+  print("b is a greater number")
+if c>a and c>b:
+  print("\n\tc is a greater number")
